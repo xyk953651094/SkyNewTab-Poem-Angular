f7308dbbd0a6685841a1c366b26b1cf69988f67f
待看云开 <xyk953651094@qq.com>
2022-12-04 20:14:22 +0800

    初次提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { lightThemeArray, darkThemeArray } from "../public/publicConstants";
+import { getFontColor } from "../public/publicFunctions";
+const $ = require('jquery');
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SkyNewTab-Poem-Angular';
+    title = 'Sky诗词新标签页 Angular';
 }
+
+// 随机颜色主题
+function setColorTheme() {
+    let hour = new Date().getHours();
+    let theme = lightThemeArray;  // 默认为亮色
+    if (18 < hour || hour < 6) {
+        theme = darkThemeArray;   // 夜间为暗色
+    }
+    let randomNum = Math.floor(Math.random() * theme.length);  // 随机选择
+
+    $("body").css('background-color', theme[randomNum].bodyBackgroundColor);
+    $(".poem-font").css({'color': theme[randomNum].fontColor});
+}
+
+setColorTheme();
+
+
+
