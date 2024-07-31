ce53b64c736cb9e64b50d625a50a6e0a59d5dea1
待看云开 <xyk953651094@qq.com>
2024-07-31 21:16:55 +0800

    修复已知问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,154 +60,149 @@ export class AppComponent implements OnInit {
         setFont(".poemFont", this.preferenceData);
 
         // 修改弹窗主题
-        let bodyEle = $("body");
-        bodyEle.bind("DOMNodeInserted", () => {
-            // 通用
-            $(".ant-list-header, .ant-list-item").css("borderBlockEndColor", getFontColor(this.minorColor));
-            $(".ant-list-header, .ant-list-item, .ant-list-footer").css("padding", "6px 0");
-            $(".ant-list-item-action").css("marginInlineStart", "0");
-            $(".ant-empty-description").css({
-                "color": getFontColor(this.minorColor),
-            }).addClass("poemFont");
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                // 插入节点时
+                if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                    // 通用
+                    $(".ant-list-header, .ant-list-item").css("borderBlockEndColor", getFontColor(this.minorColor));
+                    $(".ant-list-header, .ant-list-item, .ant-list-footer").css("padding", "6px 0");
+                    $(".ant-list-item-action").css("marginInlineStart", "0");
+                    $(".ant-empty-description").css({
+                        "color": getFontColor(this.minorColor),
+                    }).addClass("poemFont");
 
-            // popover
-            let popoverEle = $(".ant-popover");
-            if (popoverEle.length && popoverEle.length > 0) {
-                $(".ant-popover-arrow").css("display", "none");
-                // $(".ant-popover-inner").css("box-shadow", "none");
-                $(".ant-popover-inner").css("border-radius", "2px");
-                $(".ant-popover-title").css({
-                    "color": getFontColor(this.minorColor),
-                    "background-color": this.minorColor,
-                    "border-color": this.minorColor,
-                    "border-top-left-radius": "2px",
-                    "border-top-right-radius": "2px"
-                }).addClass("poemFont");
-                $(".ant-popover-inner-content").css({
-                    "color": getFontColor(this.minorColor),
-                    "background-color": this.minorColor,
-                    "border-bottom-left-radius": "2px",
-                    "border-bottom-right-radius": "2px"
-                });
-                $(".ant-select-item").addClass("poemFont");
-                $(".ant-form-item-extra").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                    // popover
+                    let popoverEle = $(".ant-popover");
+                    if (popoverEle.length && popoverEle.length > 0) {
+                        $(".ant-popover-arrow").css("display", "none");
+                        // $(".ant-popover-inner").css("box-shadow", "none");
+                        $(".ant-popover-inner").css("border-radius", "2px");
+                        $(".ant-popover-title").css({
+                            "color": getFontColor(this.minorColor),
+                            "background-color": this.minorColor,
+                            "border-color": this.minorColor,
+                            "border-top-left-radius": "2px",
+                            "border-top-right-radius": "2px"
+                        }).addClass("poemFont");
+                        $(".ant-popover-inner-content").css({
+                            "color": getFontColor(this.minorColor),
+                            "background-color": this.minorColor,
+                            "border-bottom-left-radius": "2px",
+                            "border-bottom-right-radius": "2px"
+                        });
+                        $(".ant-select-item").addClass("poemFont");
+                        $(".ant-form-item-extra").css("color", getFontColor(this.minorColor)).addClass("poemFont");
 
-                let dailyNotificationStorage = localStorage.getItem("dailyNotification");
-                if (dailyNotificationStorage) {
-                    resetSwitchColor("#dailyNotificationSwitch", JSON.parse(dailyNotificationStorage), this.majorColor);
+                        let dailyNotificationStorage = localStorage.getItem("dailyNotification");
+                        if (dailyNotificationStorage) {
+                            resetSwitchColor("#dailyNotificationSwitch", JSON.parse(dailyNotificationStorage), this.majorColor);
+                        }
+                        let todoNotificationStorage = localStorage.getItem("todoNotification");
+                        if (todoNotificationStorage) {
+                            resetSwitchColor("#todoNotificationSwitch", JSON.parse(todoNotificationStorage), this.majorColor);
+                        }
+                        let focusMode = localStorage.getItem("focusMode");
+                        if (focusMode) {
+                            resetSwitchColor("#focusModeSwitch", JSON.parse(focusMode), this.majorColor);
+                        }
+                    }
+
+                    // toolTip
+                    let toolTipEle = $(".ant-tooltip");
+                    if (toolTipEle.length && toolTipEle.length > 0) {
+                        $(".ant-tooltip-arrow").css("display", "none");
+                        $(".ant-tooltip-inner").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                    }
+
+                    // message
+                    let messageEle = $(".ant-message");
+                    if (messageEle.length && messageEle.length > 0) {
+                        $(".ant-message-notice-content").css({
+                            "backgroundColor": this.minorColor,
+                            "color": getFontColor(this.minorColor),
+                        }).addClass("poemFont");
+                        $(".ant-message-custom-content > .anticon").css({
+                            "color": getFontColor(this.minorColor),
+                        });
+                    }
+
+                    // notification
+                    let notificationEle = $(".ant-notification");
+                    if (notificationEle.length && notificationEle.length > 0) {
+                        $(".ant-notification-notice").css({"backgroundColor": this.minorColor});
+                        $(".ant-notification-notice-icon").css("color", getFontColor(this.minorColor));
+                        $(".ant-notification-notice-message").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                        $(".ant-notification-notice-description").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                    }
+
+                    // drawer
+                    let drawerEle = $(".ant-drawer");
+                    if (drawerEle.length && drawerEle.length > 0) {
+                        $(".ant-drawer-header").css({
+                            "background-color": this.minorColor,
+                            "border-bottom": "1px solid " + getFontColor(this.minorColor)
+                        });
+                        $(".ant-drawer-title").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                        $(".ant-card-head").css({
+                            "background-color": this.minorColor,
+                            "border-bottom": "2px solid " + getFontColor(this.minorColor)
+                        });
+                        $(".ant-card-head-title").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                        $(".ant-form-item-label > label").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                        $(".ant-form-item-extra").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                        $(".ant-radio-wrapper").children(":last-child").css("color", getFontColor(this.minorColor)).addClass("poemFont");
+                        $(".ant-select-item").addClass("poemFont");
+                        $(".ant-drawer-footer").css({"background-color": this.minorColor, "border-top": "1px solid " + getFontColor(this.minorColor)});
+
+                        // menuPreferenceComponent
+                        resetRadioColor(this.preferenceData.searchEngine, ["bing", "google"], this.majorColor);
+                        resetRadioColor(this.preferenceData.buttonShape, ["round", "default"], this.majorColor);
+                        resetRadioColor(this.preferenceData.poemTopic, poemTopics, this.majorColor);
+                        resetRadioColor(this.preferenceData.fontFamily, ["cursive", "sansSerif"], this.majorColor);
+                        resetRadioColor(this.preferenceData.fontVariant, ["simplified", "traditional"], this.majorColor);
+                        resetSwitchColor("#autoTopicSwitch", this.preferenceData.autoTopic, this.majorColor);
+                        resetSwitchColor("#simpleModeSwitch", this.preferenceData.simpleMode, this.majorColor);
+                    }
+
+                    // modal
+                    let modalEle = $(".ant-modal");
+                    if (modalEle.length && modalEle.length > 0) {
+                        $(".ant-modal-close-icon").css("color", getFontColor(this.minorColor))
+                        $(".ant-modal-header").css({
+                            "background-color": this.minorColor,
+                            "border-color": this.minorColor
+                        });
+                        $(".ant-modal-title").css({
+                            "background-color": this.minorColor,
+                            "color": getFontColor(this.minorColor),
+                        }).addClass("poemFont");
+                        $(".ant-modal-footer").css({
+                            "backgroundColor": this.minorColor,
+                            "border-color": this.minorColor
+                        });
+                        $(".ant-modal-footer > .ant-btn").css("color", getFontColor(this.minorColor)).addClass("poemFont")
+                        if (this.preferenceData.buttonShape === "round") {
+                            $(".ant-modal-footer > .ant-btn").removeClass("ant-btn-default ant-btn-primary").addClass("poemFont ant-btn-round ant-btn-text");
+                        } else {
+                            $(".ant-modal-footer > .ant-btn").removeClass("ant-btn-round ant-btn-default ant-btn-primary").addClass("poemFont ant-btn-text");
+                        }
+                        $(".ant-modal-footer > .ant-btn").on("mouseover", (e: any) => {
+                            e.currentTarget.style.backgroundColor = this.majorColor;
+                            e.currentTarget.style.color = getFontColor(this.majorColor);
+                        });
+                        $(".ant-modal-footer > .ant-btn").on("mouseout", (e: any) => {
+                            e.currentTarget.style.backgroundColor = "transparent";
+                            e.currentTarget.style.color = getFontColor(this.minorColor);
+                        });
+                    }
+
+                    // 设置字体(需要优化)
+                    setFont(".poemFont", this.preferenceData);
                 }
-                let todoNotificationStorage = localStorage.getItem("todoNotification");
-                if (todoNotificationStorage) {
-                    resetSwitchColor("#todoNotificationSwitch", JSON.parse(todoNotificationStorage), this.majorColor);
-                }
-                let focusMode = localStorage.getItem("focusMode");
-                if (focusMode) {
-                    resetSwitchColor("#focusModeSwitch", JSON.parse(focusMode), this.majorColor);
-                }
-            }
-
-            // toolTip
-            let toolTipEle = $(".ant-tooltip");
-            if (toolTipEle.length && toolTipEle.length > 0) {
-                $(".ant-tooltip-arrow").css("display", "none");
-                $(".ant-tooltip-inner").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-            }
-
-            // message
-            let messageEle = $(".ant-message");
-            if (messageEle.length && messageEle.length > 0) {
-                $(".ant-message-notice-content").css({
-                    "backgroundColor": this.minorColor,
-                    "color": getFontColor(this.minorColor),
-                }).addClass("poemFont");
-                $(".ant-message-custom-content > .anticon").css({
-                    "color": getFontColor(this.minorColor),
-                });
-            }
-
-            // notification
-            let notificationEle = $(".ant-notification");
-            if (notificationEle.length && notificationEle.length > 0) {
-                $(".ant-notification-notice").css({"backgroundColor": this.minorColor});
-                $(".ant-notification-notice-icon").css("color", getFontColor(this.minorColor));
-                $(".ant-notification-notice-message").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-                $(".ant-notification-notice-description").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-            }
-
-            // drawer
-            let drawerEle = $(".ant-drawer");
-            if (drawerEle.length && drawerEle.length > 0) {
-                $(".ant-drawer-header").css({
-                    "background-color": this.minorColor,
-                    "border-bottom": "1px solid " + getFontColor(this.minorColor)
-                });
-                $(".ant-drawer-title").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-                $(".ant-card-head").css({
-                    "background-color": this.minorColor,
-                    "border-bottom": "2px solid " + getFontColor(this.minorColor)
-                });
-                $(".ant-card-head-title").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-                $(".ant-form-item-label > label").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-                $(".ant-form-item-extra").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-                $(".ant-radio-wrapper").children(":last-child").css("color", getFontColor(this.minorColor)).addClass("poemFont");
-                $(".ant-select-item").addClass("poemFont");
-                $(".ant-drawer-footer").css({"background-color": this.minorColor, "border-top": "1px solid " + getFontColor(this.minorColor)});
-
-                // menuPreferenceComponent
-                resetRadioColor(this.preferenceData.searchEngine, ["bing", "google"], this.majorColor);
-                resetRadioColor(this.preferenceData.buttonShape, ["round", "default"], this.majorColor);
-                resetRadioColor(this.preferenceData.poemTopic, poemTopics, this.majorColor);
-                resetRadioColor(this.preferenceData.fontFamily, ["cursive", "sansSerif"], this.majorColor);
-                resetRadioColor(this.preferenceData.fontVariant, ["simplified", "traditional"], this.majorColor);
-                resetSwitchColor("#autoTopicSwitch", this.preferenceData.autoTopic, this.majorColor);
-                resetSwitchColor("#simpleModeSwitch", this.preferenceData.simpleMode, this.majorColor);
-            }
-
-            // modal
-            let modalEle = $(".ant-modal");
-            if (modalEle.length && modalEle.length > 0) {
-                $(".ant-modal-close-icon").css("color", getFontColor(this.minorColor))
-                $(".ant-modal-header").css({
-                    "background-color": this.minorColor,
-                    "border-color": this.minorColor
-                });
-                $(".ant-modal-title").css({
-                    "background-color": this.minorColor,
-                    "color": getFontColor(this.minorColor),
-                }).addClass("poemFont");
-                $(".ant-modal-footer").css({
-                    "backgroundColor": this.minorColor,
-                    "border-color": this.minorColor
-                });
-                $(".ant-modal-footer > .ant-btn").css("color", getFontColor(this.minorColor)).addClass("poemFont")
-                if (this.preferenceData.buttonShape === "round") {
-                    $(".ant-modal-footer > .ant-btn").removeClass("ant-btn-default ant-btn-primary").addClass("poemFont ant-btn-round ant-btn-text");
-                } else {
-                    $(".ant-modal-footer > .ant-btn").removeClass("ant-btn-round ant-btn-default ant-btn-primary").addClass("poemFont ant-btn-text");
-                }
-                $(".ant-modal-footer > .ant-btn").on("mouseover", (e: any) => {
-                    e.currentTarget.style.backgroundColor = this.majorColor;
-                    e.currentTarget.style.color = getFontColor(this.majorColor);
-                });
-                $(".ant-modal-footer > .ant-btn").on("mouseout", (e: any) => {
-                    e.currentTarget.style.backgroundColor = "transparent";
-                    e.currentTarget.style.color = getFontColor(this.minorColor);
-                });
-            }
-
-            // 设置字体(需要优化)
-            setFont(".poemFont", this.preferenceData);
+            });
         });
-
-        // const observer = new MutationObserver((mutations) => {
-        //     mutations.forEach((mutation) => {
-        //         // 插入节点时
-        //         if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-        //
-        //         }
-        //     });
-        // });
-        // observer.observe(document.body, {childList: true});
+        observer.observe(document.body, {childList: true});
     }
 
     ngOnInit(): void {
